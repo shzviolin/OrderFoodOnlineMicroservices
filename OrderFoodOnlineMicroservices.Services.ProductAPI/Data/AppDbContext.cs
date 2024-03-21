591e1795f8c05ae983eb88823e5382ab1aab5f29
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-03-21 19:00:56 +0330

    16_Create OrderFoodOnlineMicroservices.Services.ProductAPI_Configure AppDbContext and Program.cs_Seed data

---
+++ OrderFoodOnlineMicroservices.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using OrderFoodOnlineMicroservices.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderFoodOnlineMicroservices.Services.ProductAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Fish and Chips",
+                Price = 15,
+                Description = "Crispy fish served with hot chips, a classic dish loved by many.<br/> Perfect for a satisfying meal.",
+                ImageUrl = "https://placehold.co/603x403",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Shepherd's Pie",
+                Price = 13.99,
+                Description = "A hearty dish with minced meat and mashed potatoes on top.<br/> Comfort food at its best.",
+                ImageUrl = "https://placehold.co/602x402",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Apple Pie",
+                Price = 10.99,
+                Description = "A delicious pie filled with sweet apples and cinnamon.<br/> Perfect for dessert lovers.",
+                ImageUrl = "https://placehold.co/601x401",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Roast Beef",
+                Price = 15,
+                Description = "Tender slices of roast beef served with gravy and vegetables.<br/> A classic entree choice.",
+                ImageUrl = "https://placehold.co/600x400",
+                CategoryName = "Entree"
+            });
+
+        }
+    }
+}
---
+++ OrderFoodOnlineMicroservices.Services.ProductAPI/Migrations/20240321152815_Create_ProductsTbl_SeedData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrderFoodOnlineMicroservices.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_ProductsTbl_SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Crispy fish served with hot chips, a classic dish loved by many.<br/> Perfect for a satisfying meal.", "https://placehold.co/603x403", "Fish and Chips", 15.0 },
+                    { 2, "Appetizer", "A hearty dish with minced meat and mashed potatoes on top.<br/> Comfort food at its best.", "https://placehold.co/602x402", "Shepherd's Pie", 13.99 },
+                    { 3, "Dessert", "A delicious pie filled with sweet apples and cinnamon.<br/> Perfect for dessert lovers.", "https://placehold.co/601x401", "Apple Pie", 10.99 },
+                    { 4, "Entree", "Tender slices of roast beef served with gravy and vegetables.<br/> A classic entree choice.", "https://placehold.co/600x400", "Roast Beef", 15.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
