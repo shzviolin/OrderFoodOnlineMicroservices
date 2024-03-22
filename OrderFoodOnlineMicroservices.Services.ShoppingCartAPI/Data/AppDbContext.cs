a466565e21ae0663550d732806d29fd17d67465b
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-03-22 12:09:56 +0330

    20_Create OrderFoodOnlineMicroservices.Services.ShoppingCartAPI_Configure AppDbContext and Program.cs_Seed data_Create Models and Dtos

---
+++ OrderFoodOnlineMicroservices.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models;
+
+namespace OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+    }
+}
---
+++ OrderFoodOnlineMicroservices.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models.Dto;
+
+namespace OrderFoodOnlineMicroservices.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
