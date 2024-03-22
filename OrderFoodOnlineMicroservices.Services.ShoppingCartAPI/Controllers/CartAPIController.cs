1f5633247d1ed1035354fbada8a9eedfbfea500e
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-03-22 14:34:17 +0330

    21_Implement ShoppingCartAPIs

---
+++ OrderFoodOnlineMicroservices.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,134 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Data;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models;
+using OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models.Dto;
+using System.Reflection.PortableExecutable;
+
+namespace OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        private ResponseDto _response;
+
+        public CartAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<ResponseDto> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cart = new()
+                {
+                    CartHeader = _mapper.Map<CartHeaderDto>(_db.CartHeaders.First(x => x.UserId == userId))
+                };
+                cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails.Where(x => x.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+                foreach (var item in cart.CartDetails)
+                {
+                    cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message.ToString();
+            }
+
+            return _response;
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(x => x.UserId == cartDto.CartHeader.UserId);
+                if (cartHeaderFromDb == null)
+                {
+                    //create header and details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    _db.CartHeaders.Add(cartHeader);
+                    await _db.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+                    _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    //if header is not null
+                    //check if details has same product
+                    var cartdetailsFromDb = await _db.CartDetails.AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.ProductId == cartDto.CartDetails.First().ProductId &&
+                                                  x.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                    if (cartdetailsFromDb == null)
+                    {
+                        //create cartdetails
+                        cartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+                        _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        //update count in cart details
+                        cartDto.CartDetails.First().Count += cartdetailsFromDb.Count;
+                        cartDto.CartDetails.First().CartHeaderId = cartdetailsFromDb.CartHeaderId;
+                        cartDto.CartDetails.First().CartDetailsId = cartdetailsFromDb.CartDetailsId;
+
+                        _db.CartDetails.Update(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
+                        await _db.SaveChangesAsync();
+                    }
+
+                    _response.Result = cartDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message.ToString();
+            }
+
+            return _response;
+        }
+
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart([FromBody] int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = _db.CartDetails.First(x => x.CartDetailsId == cartDetailsId);
+
+                int totalCountOfCartitem = _db.CartDetails.Where(x => x.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+                if (totalCountOfCartitem == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders.FirstOrDefaultAsync(x => x.CartHeaderId == cartDetails.CartHeaderId);
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message.ToString();
+            }
+
+            return _response;
+        }
+    }
+}
--- OrderFoodOnlineMicroservices.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
+++ OrderFoodOnlineMicroservices.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -9,7 +9,7 @@ namespace OrderFoodOnlineMicroservices.Services.ShoppingCartAPI.Models.Dto
         public int CartHeaderId { get; set; }
         public CartHeaderDto? CartHeader { get; set; }
         public int ProductId { get; set; }
-        public ProductDto Product { get; set; }
+        public ProductDto? Product { get; set; }
         public int Count { get; set; }
 
     }
